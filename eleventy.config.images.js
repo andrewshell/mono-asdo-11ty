4391178a81f689e0e67329bdfb51d9c3ecbefef1
Andrew Shell <andrew@andrewshell.org>
2024-04-17 09:35:26 -0500

    Improving image semantics and adding thumbnail to index pages

--- eleventy.config.images.js
+++ eleventy.config.images.js
@@ -2,8 +2,8 @@ const path = require("path");
 const eleventyImage = require("@11ty/eleventy-img");
 
 module.exports = eleventyConfig => {
-	function relativeToRootPath(relativeFilePath) {
-		return path.resolve(__dirname, relativeFilePath);
+	function relativeToPublicPath(relativeFilePath) {
+		return path.resolve(__dirname, './public', `./${relativeFilePath}`);
 	}
 
 	// Eleventy Image shortcode
@@ -12,7 +12,7 @@ module.exports = eleventyConfig => {
 		// Full list of formats here: https://www.11ty.dev/docs/plugins/image/#output-formats
 		// Warning: Avif can be resource-intensive so take care!
 		let formats = ["avif", "webp", "auto"];
-		let file = relativeToRootPath(src);
+		let file = relativeToPublicPath(src);
 
 		let metadata = await eleventyImage(file, {
 			widths: widths || ["auto"],
@@ -20,15 +20,22 @@ module.exports = eleventyConfig => {
 			outputDir: path.join(eleventyConfig.dir.output, "img"), // Advanced usage note: `eleventyConfig.dir` works here because we’re using addPlugin.
 		});
 
-		console.dir(sizes);
-
 		// TODO loading=eager and fetchpriority=high
 		let imageAttributes = {
 			alt,
 			sizes,
+      itemprop: "contentUrl",
 			loading: "lazy",
 			decoding: "async",
 		};
-		return eleventyImage.generateHTML(metadata, imageAttributes);
+
+    let options = {
+      pictureAttributes: {
+        itemscope: true,
+        itemtype: "http://schema.org/ImageObject"
+      },
+    };
+
+		return eleventyImage.generateHTML(metadata, imageAttributes, options);
 	});
 };
